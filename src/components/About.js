cb4175e68f3a1a47a2f326e03fc2f45a794914c9
Layna Oyervides <laynaoyervides@gmail.com>
2021-12-02 14:53:16 -0700

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React from "react"
+import Links from "./Links"
+import user from "../data/user"
 
-function About() {
+
+function About(props) {
+  if (!props.bio) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <Links github={user.links.github} linkedin={user.links.linkedin} />
+      </div>
+    );
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
+  }
 }
 
 export default About;
